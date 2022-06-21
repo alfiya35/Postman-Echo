a4635f2f6d488f82da5089f9a144e188fd14afc7
Альфия Устименко <alfiya.ustimenko@ukr.net>
2022-06-21 15:28:38 +0500

    заваленый тест

--- src/test/java/ru/netology/test/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/test/MobileBankApiTestV1.java
@@ -23,7 +23,7 @@ public class MobileBankApiTestV1 {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data",equalTo("some data"))
+                .body("data",equalTo("USD"))
         ;
 
     }
